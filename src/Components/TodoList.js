380ada91e384299f0452ba6a72324965e42a3870
Lauren Hess <laurenbethhess@gmail.com>
2022-01-03 10:50:18 -0500

    changed color by importance

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,10 +1,23 @@
 import React, { useEffect, useState } from 'react'
 
 function TodoList({todo}) {
+    
+
+    
+            
+
+
 
     return (
         <div>
-            <li>{todo.item.toLowerCase()}</li>
+            {todo.importance == "high" ? (
+                <li className="red">{todo.item.toLowerCase()}</li>
+            ) : todo.importance == "medium" ? (
+                <li className="orange">{todo.item.toLowerCase()}</li>
+            ) : (
+                <li className="green" >{todo.item.toLowerCase()}</li>   
+            )}
+
         </div>
     )
 }
