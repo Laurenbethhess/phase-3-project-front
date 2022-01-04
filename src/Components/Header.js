6806bdeae8135d5507a91f4240a42e9fcb3805bd
Lauren Hess <laurenbethhess@gmail.com>
2022-01-03 19:22:01 -0500

    added filter to todolist

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,15 +2,34 @@ import React, { useEffect, useState } from 'react'
 import TodoList from './TodoList'
 
 function Header({todos, onTodoDelete}) {
-    const renderTodosList = todos.map(todo => <TodoList todo={todo} key={todo.id} onTodoDelete={onTodoDelete}/>)
-
+    const [filterBy, setFilterBy] = useState('')
+    const filteredTodos = todos.filter(todo => todo.category.category === filterBy) 
+    const renderTodosList = filteredTodos.map(todo => <TodoList todo={todo} key={todo.id} onTodoDelete={onTodoDelete}/>)
+    const renderTodosListAll = todos.map(todo => <TodoList todo={todo} key={todo.id} onTodoDelete={onTodoDelete}/>)
+    
+    const onFilterChange = (e) => {
+        setFilterBy(e.target.value)
+      }
 
     return (
         <div>
             <h1>To-Do List</h1>
+            <h2>All Todos</h2>
+            {renderTodosListAll}
+            <h2>Filter:</h2>
+            <select onChange={onFilterChange} value={filterBy}>
+                <option value="not assigned">Not Assigned</option>
+                <option value="work">Work</option>
+                <option value="chores">Chores</option>
+                <option value="exercise">Exercise</option>
+                <option value="misc">Misc</option>
+                <option value="groceries">Groceries</option>
+                <option value="kids">Kids</option>
+            </select>
             {renderTodosList}
-            <>_______________________________________________</>            
+         
             <br/>
+            <>_______________________________________________</>
         </div>
     )
 }
